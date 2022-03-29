8b4f6e7026eb596c5e7532feae8d5fa525ceb0de
Bryan Jonker <jonker@illinois.edu>
2022-03-29 10:31:01 -0500

    Adding UI improvements, adding delete function

--- DigitalSignEditor/ApiModels/SignGroupItem.cs
+++ DigitalSignEditor/ApiModels/SignGroupItem.cs
@@ -5,6 +5,7 @@
         public string Description { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
+        public string SignType { get; set; }
         public string Url { get; set; }
     }
 }
