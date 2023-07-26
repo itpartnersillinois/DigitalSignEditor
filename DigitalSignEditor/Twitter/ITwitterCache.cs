370bf477cf2eabdc2e01637a517545bb548f18e0
Bryan Jonker <jonker@illinois.edu>
2023-07-26 13:16:09 -0500

    Remove twitter option

--- DigitalSignEditor/Twitter/ITwitterCache.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DigitalSignEditor.Twitter {
-
-    public interface ITwitterCache {
-
-        bool AddCache(string username, string data);
-
-        string GetCache(string username);
-    }
-}
